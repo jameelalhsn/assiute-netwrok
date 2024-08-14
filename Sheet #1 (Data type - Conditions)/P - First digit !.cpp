7e131df7cd3945ce9e6a506a3315a939f53447a4
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:40:35 +0300

    Update P - First digit !.cpp

--- Sheet #1 (Data type - Conditions)/P - First digit !.cpp
+++ Sheet #1 (Data type - Conditions)/P - First digit !.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <array>
 #include <cmath>
 using namespace std ;
-int main(){
-int a,b;
-cin >> a >> b;
-if (a > b ){
-    cout << "Yes"<<endl;
-}else if (a == b){
-    cout << "Yes"<<endl;
-}else if (a<b){
-    cout << "No"<<endl;
-}
  
+int main()
+ {
+int num ; 
+cin >> num ;
+int res = num /1000;
+if(res %2 ==0){
+     cout<<"EVEN";
+}else {cout <<"ODD";}
+return 0 ;
  }
