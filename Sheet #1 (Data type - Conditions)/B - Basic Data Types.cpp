b44e096a1e9cf1365de629d12868912e7b35acca
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 20:29:44 +0300

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/B - Basic Data Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int integer;
+    long long lng;
+    char ch;
+    float flt;
+    double dbl;
+    cin >> integer >> lng >> ch >> flt >> dbl ;
+    cout << integer << endl << lng << endl << ch << endl << flt << endl << dbl ;
+    return 0 ;
+}
