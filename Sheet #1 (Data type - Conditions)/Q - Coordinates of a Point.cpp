e8d799f1d8c96b52af74dd6f731d6626045c730a
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:41:19 +0300

    Update Q - Coordinates of a Point.cpp

--- Sheet #1 (Data type - Conditions)/Q - Coordinates of a Point.cpp
+++ Sheet #1 (Data type - Conditions)/Q - Coordinates of a Point.cpp
@@ -1,15 +1,25 @@
+ 
 #include <iostream>
+#include <array>
 #include <cmath>
 using namespace std ;
 int main(){
-int a,b;
-cin >> a >> b;
-if (a > b ){
-    cout << "Yes"<<endl;
-}else if (a == b){
-    cout << "Yes"<<endl;
-}else if (a<b){
-    cout << "No"<<endl;
+double x ,y ;
+cin >> x >> y ;
+if(x>0 && y>0){
+     cout<<"Q1";
+}else if(x<0 && y>0){
+     cout<<"Q2";
+}else if(x<0 && y<0){
+     cout<<"Q3";
+}else if(x>0 && y<0){
+     cout<<"Q4";
+}else if (x==0 && y==0){
+     cout<<"Origem";
+}else if (x==0 ){
+     cout << "Eixo Y";
+}else if (y==0 ){
+     cout << "Eixo X";
 }
- 
+return 0 ;
  }
