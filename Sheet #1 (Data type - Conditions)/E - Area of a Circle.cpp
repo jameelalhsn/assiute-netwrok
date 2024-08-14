b052c3885e812cfd36ca534baa608ce7caabef94
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:35:16 +0300

    Add files via upload

---
+++ Sheet #1 (Data type - Conditions)/E - Area of a Circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<iomanip>
+using namespace std ;
+int main()
+{
+cout << fixed << setprecision(9);
+double π=3.141592653 ;
+double R;cin >> R ;
+cout << R*R*π ;
+    return 0 ;
+}
---
+++ Sheet #1 (Data type - Conditions)/Y - The last 2 digits.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+int main(){
+int a,b;
+cin >> a >> b;
+if (a > b ){
+    cout << "Yes"<<endl;
+}else if (a == b){
+    cout << "Yes"<<endl;
+}else if (a<b){
+    cout << "No"<<endl;
+}
+ 
+ }
