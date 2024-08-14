6294c6c36bbca5bbbbca036bf4b030ec6c6d2f83
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:37:23 +0300

    Update L - The Brothers.cpp

--- Sheet #1 (Data type - Conditions)/L - The Brothers.cpp
+++ Sheet #1 (Data type - Conditions)/L - The Brothers.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <cmath>
 using namespace std ;
-int main(){
-int a,b;
-cin >> a >> b;
-if (a > b ){
-    cout << "Yes"<<endl;
-}else if (a == b){
-    cout << "Yes"<<endl;
-}else if (a<b){
-    cout << "No"<<endl;
+int main() 
+{
+string namef1 ,names1,namef2 ,names2 ;
+cin >> namef1 >> names1 >> namef2 >> names2 ;
+if(names1 == names2){
+    cout << "ARE Brothers";
+} else {cout<<"NOT";}
 }
- 
- }
