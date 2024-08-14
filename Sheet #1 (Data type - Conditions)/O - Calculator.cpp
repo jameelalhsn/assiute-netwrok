cdd3c8724913eb4979bbf6841bd18bbd18565b5a
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:39:55 +0300

    Update O - Calculator.cpp

--- Sheet #1 (Data type - Conditions)/O - Calculator.cpp
+++ Sheet #1 (Data type - Conditions)/O - Calculator.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <array>
 #include <cmath>
 using namespace std ;
 int main(){
-int a,b;
-cin >> a >> b;
-if (a > b ){
-    cout << "Yes"<<endl;
-}else if (a == b){
-    cout << "Yes"<<endl;
-}else if (a<b){
-    cout << "No"<<endl;
+int num1 , num2 ;
+char op;
+cin >> num1 >> op >>num2;
+if (int (op)==42){// *
+cout <<num1*num2;
+}else if (int (op)==43){// +
+cout <<num1+num2;
+}else if (int (op)==47){// /
+cout <<num1/num2;
+}else if (int (op)==45){// -
+cout <<num1-num2;
 }
- 
  }
