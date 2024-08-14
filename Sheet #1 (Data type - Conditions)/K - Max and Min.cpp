9b32f1be92f47570de64e0883954f10af4b758f0
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:36:39 +0300

    Update K - Max and Min.cpp

--- Sheet #1 (Data type - Conditions)/K - Max and Min.cpp
+++ Sheet #1 (Data type - Conditions)/K - Max and Min.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <cmath>
 using namespace std ;
-int main(){
-int a,b;
-cin >> a >> b;
-if (a > b ){
-    cout << "Yes"<<endl;
-}else if (a == b){
-    cout << "Yes"<<endl;
-}else if (a<b){
-    cout << "No"<<endl;
-}
- 
+int main ()
+ { 
+  int num1 ,num2 ,num3 ;
+  cin >> num1 >> num2 >> num3 ;
+    if (num1 >= num2 && num1 >= num3){//num1
+  
+    if (num2>num3)
+      cout <<num3 <<" "<<num1 ;
+        else {cout << num2 << " "<< num1;}
+    }else if (num2 >= num1 && num2 >= num3){//num2
+    if (num1>num3)
+      cout << num3 <<" "<<num2 ;
+        else { cout << num1 << " "<< num2;}
+    
+  }else if ( num3 >= num1 && num3  >=num2 ){//num3
+      if (num1 >num2)
+      cout <<num2 << " "<< num3 ;
+      else {cout <<num1 << " "<< num3;}
+  }
+ return 0 ;
  }
