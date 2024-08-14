f86db3d6e4c95e889f8cc74e8ab3ca04ed08ccc6
jameelalhsn <alhsnjmyl18@gmail.com>
2024-08-14 23:00:18 +0300

    Update C - Simple Calculator.cpp

--- C - Simple Calculator.cpp
+++ C - Simple Calculator.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
- 
 using namespace std ;
 int main()
- 
 {
- 
-    long long Y;
-    long long X;
-    cin >> X >> Y ;
+long long Y;
+long long X;
+cin >> X >> Y ;
 cout << X << " + " << Y << " = " << X+Y << endl;
 cout << X << " * " << Y << " = " << X*Y << endl;
 cout << X << " - " << Y << " = " << X-Y  ;
